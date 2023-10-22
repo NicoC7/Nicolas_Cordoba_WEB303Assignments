5a36755439dda5e9c8e335dc432f4ce28df03a04
NicoC7 <W0823791@myscc.ca>
2023-10-22 11:09:21 -0400

    Constructor

--- WEB303As05/script.js
+++ WEB303As05/script.js
@@ -5,8 +5,11 @@
 class ContentItem {
     constructor(id, name, description, categoryGenre) {
         this.id = id;
+        this.name = name;
         this.description = description;
+        this.categoryGenre = categoryGenre;
     }
+    
 
 }
 
