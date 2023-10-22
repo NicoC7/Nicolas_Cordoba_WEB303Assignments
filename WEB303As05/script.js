ef29fb4827de06efdc5d4ed6a53656794dad22cd
NicoC7 <W0823791@myscc.ca>
2023-10-22 11:07:49 -0400

    Adding the homework

---
+++ WEB303As05/script.js
@@ -0,0 +1,20 @@
+/*
+    Assignment 05
+*/
+
+class ContentItem {
+    constructor(id, name, description, categoryGenre) {
+        this.id = id;
+        this.description = description;
+    }
+
+}
+
+$(document).ready(function () {
+    // your code here
+
+});
+
+
+
+
