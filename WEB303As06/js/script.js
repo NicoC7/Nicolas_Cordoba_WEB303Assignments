493352c6d4b92ad2e4806acf049c727eedd541c5
NicoC7 <W0823791@myscc.ca>
2023-11-05 16:37:45 -0500

    Move Json file, Styling to the table and creting the Jquery ajax method to sort table.

--- WEB303As06/js/script.js
+++ WEB303As06/js/script.js
@@ -11,3 +11,80 @@ $(document).ready(function() {
         $('.tab-content').not(target).hide();
     });
 });
+
+$(document).ready(function() {
+    var originalData;
+
+    $.ajax({
+        url: 'characters.json',
+        dataType: 'json',
+        success: function(data) {
+            originalData = data.slice();
+
+            var tableBody = $('#character-table tbody');
+
+            data.forEach(function(character) {
+                tableBody.append(
+                    '<tr>' +
+                    '<td>' + character.firstName + '</td>' +
+                    '<td>' + character.lastName + '</td>' +
+                    '<td>' + character.dateOfBirth + '</td>' +
+                    '<td>' + character.occupation + '</td>' +
+                    '<td>' + character.bestFriend + '</td>' +
+                    '<td>' + character.notableQuotes.join('<br>') + '</td>' +
+                    '</tr>'
+                );
+            });
+
+          
+            $('th a').click(function() {
+                var column = $(this).data('column');
+                var order = $(this).data('order') || 'asc';
+
+                if (order === 'reset') {
+                   
+                    data = originalData.slice();
+                    order = 'asc';
+                } else {
+                    data.sort(function(a, b) {
+                        if (order === 'asc') {
+                            return a[column] > b[column] ? 1 : -1;
+                        } else {
+                            return a[column] < b[column] ? 1 : -1;
+                        }
+                    });
+                    order = order === 'asc' ? 'desc' : 'reset';
+                }
+
+                
+                $('th a .chevron').html('');
+                if (order === 'reset') {
+                    $(this).find('.chevron').html('&#x25B2'); 
+                } else if (order === 'desc') {
+                    $(this).find('.chevron').html('&#x25BC'); 
+                }
+
+              
+                tableBody.empty();
+
+                data.forEach(function(character) {
+                    tableBody.append(
+                        '<tr>' +
+                        '<td>' + character.firstName + '</td>' +
+                        '<td>' + character.lastName + '</td>' +
+                        '<td>' + character.dateOfBirth + '</td>' +
+                        '<td>' + character.occupation + '</td>' +
+                        '<td>' + character.bestFriend + '</td>' +
+                        '<td>' + character.notableQuotes.join('<br>') + '</td>' +
+                        '</tr>'
+                    );
+                });
+
+                $(this).data('order', order);
+            });
+        },
+        error: function() {
+            console.log('Failed to load data.');
+        }
+    });
+});
